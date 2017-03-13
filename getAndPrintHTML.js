7183c67fc96eec956cf89583d07bb44fa4a1bad2
Wyatt Robinson <thewyatt@gmail.com>
2017-03-13 19:46:41 +0000

    Added getAndPrintHTML function to store response chunks in string

--- getAndPrintHTML.js
+++ getAndPrintHTML.js
@@ -1,19 +1,25 @@
 var https = require("https")
-
 var requestOptions = {
 host: 'sytantris.github.io',
 path: '/http-examples/step1.html'
 };
 
-https.get(requestOptions, function getAndPrintHTMLChunks (response) {
+var stored = '';
+
+https.get(requestOptions, function (response) {
   response.setEncoding('utf8');
   response.on('data', function(data) {
-    console.log('Chunk received. Length: ' + data.length);
-    console.log(data.toString());
+    stored += data;
+    //console.log('Chunk received. Length: ' + data.length);
+    //console.log(data.toString());
   });
   response.on('end', function(){
     console.log('Response stream complete');
+    console.log(stored);
   });
 });
+
+
+
 
 
